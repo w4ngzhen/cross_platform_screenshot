afe7cdf28e772fbe44a276542c2e668af0f3a352
w4ngzhen <w4ngzhen@hotmail.com>
2023-09-26 08:52:48 +0800

    feat: 增加Example。

---
+++ src/an_example.cpp
@@ -0,0 +1,20 @@
+#include "screenshot.h"
+#include <cstdio>
+#include <ctime>
+
+int main() {
+
+  clock_t start = clock();
+
+  unsigned char *screenshot_bytes;
+  unsigned int width, height, length;
+  cps::GetScreenshotImageByteData(&screenshot_bytes, &width, &height, &length);
+
+  printf("get screenshot image data take about %f seconds.", ((double)(clock() - start)) / CLOCKS_PER_SEC);
+
+  // write image data to file.
+  FILE *file = fopen("screenshot_example.jpeg", "w+");
+  fwrite(screenshot_bytes, length, 1, file);
+  fclose(file);
+
+}
--- src/screenshot.h
+++ src/screenshot.h
@@ -4,10 +4,15 @@
 namespace cps {
 /**
  * Take a screenshot of the screen where the mouse is located
+ *
+ * @notice the screenshot image data is the JPEG formatted data.
+ *
  * @param image_bytes the pointer to screenshot image data
  * @param width image width.
  * @param height image height.
  * @param bytes_len image bytes length.
+ *
+ *
  * @return
  */
 bool GetScreenshotImageByteData(unsigned char **image_bytes,
